809246ca0ba3a8a2ca56ba9fd484bc1609c14e1e
David Wu <davidjwu92@gmail.com>
2020-07-31 17:00:20 -0700

    html work, initialized vars

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,21 @@
 //this whole file is JAVASCRIPT!
 console.log("Page loaded.")
+
+var wins = 0
+var losses = 0
+var targetScore
+var gem1
+var gem2
+var gem3
+var gem4
+var totalScore
+
+var buttonClick = function(num){
+  console.log(num)
+  var gems = [gem1, gem2, gem3, gem4]
+
+  totalScore += gems[num - 1]
+
+}
+
+
